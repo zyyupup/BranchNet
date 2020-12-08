8f0c3f2e4ccb5f6e825469787f548ed67ed08b70
zyyupup <1683474166@qq.com>
2020-12-09 01:24:03 +0800

    Update imagenet_resnet.py

--- model/imagenet_resnet.py
+++ model/imagenet_resnet.py
@@ -92,7 +92,6 @@ class ResNet(nn.Module):
         self.block = block
         super(ResNet, self).__init__()
         self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,bias=False)
-        self.conv1 = nn.Conv2d(3, 64, padding=1,kernel_size=3,bias=False)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.bn1 = nn.BatchNorm2d(64)
         self.relu = nn.ReLU(inplace=True)
